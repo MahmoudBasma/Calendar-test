cfc522b4383758324b6c79fb5dcca2b0e6225340
Mahmoud Basma <mahmoud.basma@lau.edu>
2022-02-23 17:57:44 +0200

    updates

--- second-test/src/App.js
+++ second-test/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Calendar, Views } from 'react-big-calendar'
+import events from '../events'
+import * as dates from '../../src/utils/dates'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+let allViews = Object.keys(Views).map(k => Views[k])
 
-export default App;
+const ColoredDateCellWrapper = ({ children }) =>
+  React.cloneElement(React.Children.only(children), {
+    style: {
+      backgroundColor: 'lightblue',
+    },
+  })
+
+let App = ({ localizer }) => (
+  <Calendar
+    events={events}
+    views={allViews}
+    step={60}
+    showMultiDayTimes
+    max={dates.add(dates.endOf(new Date(2015, 17, 1), 'day'), -1, 'hours')}
+    defaultDate={new Date(2015, 3, 1)}
+    components={{
+      timeSlotWrapper: ColoredDateCellWrapper,
+    }}
+    localizer={localizer}
+  />
+)
+
+export default Basic
